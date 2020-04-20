fec5839d8e8b8257bfeb4e83b06b0f10f3bf47de
schultzm <dr.mark.schultz@gmail.com>
2020-04-20 13:29:31 +1000

    building SRA_table class

--- gnb/utils/sra_builder.py
+++ gnb/utils/sra_builder.py
@@ -1,3 +1,5 @@
 class SRA_table:
-    def __init__(self):
-        pass
+    def __init__(self, sra_template, biosample_attributes, gisaid_upload):
+        self.sra_template = sra_template
+        self.attributes_bsmpls = biosample_attributes
+        self. gisaid_upload = gisaid_upload
